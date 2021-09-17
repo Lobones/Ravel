35fb8cf23b8dcce4b513692e555b1079ad94ab41
Lobones <romulolfbsilva@gmail.com>
2021-09-17 18:54:24 -0400

    front-end initialize

--- libraries/settings.py
+++ libraries/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_ravel.urls'
+ROOT_URLCONF = 'libraries.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_ravel.wsgi.application'
+WSGI_APPLICATION = 'libraries.wsgi.application'
 
 
 # Database
